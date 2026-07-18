7d2b97405c59298ca325b7d9a4aa0b3a4470ca79
quellodellasicurezza <quellodellasicurezza@gmail.com>
2026-07-18 19:47:54 +0200

    Update update-substack.js

--- scripts/update-substack.js
+++ scripts/update-substack.js
@@ -1,1 +1,40 @@
+const fs = require("fs");
+const Parser = require("rss-parser");
 
+const parser = new Parser();
+
+const FEED =
+  "https://quellodellasicurezza.substack.com/feed";
+
+async function update() {
+
+  const feed = await parser.parseURL(FEED);
+
+  const post = feed.items[0];
+
+  const title = post.title || "";
+  const link = post.link || "";
+  const date = new Date(post.pubDate)
+    .toLocaleDateString("it-IT");
+
+  const html = `
+<div class="widget-title">
+Ultima analisi
+</div>
+
+<a href="${link}" target="_blank">
+<strong>${title}</strong>
+<br><br>
+<span style="color:#aaa;font-size:13px">
+Pubblicata il ${date}
+</span>
+</a>
+`;
+
+  fs.writeFileSync(
+    "latest-newsletter.html",
+    html
+  );
+}
+
+update();
